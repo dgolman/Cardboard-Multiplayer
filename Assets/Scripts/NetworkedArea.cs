9569e89828552011542e59268a7c3f9becf688a4
Daniel Golman <dgolman@vt.edu>
2016-01-07 15:08:51 -0500

    initial commit

---
+++ Assets/Scripts/NetworkedArea.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkedArea : Photon.MonoBehaviour
+{
+	public GameObject plane;
+	public GameObject sphere;
+
+	public Transform areaGlobal;
+	public Transform planeLocal;
+	public Transform sphereLocal;
+
+	void Start ()
+	{
+		Debug.Log("i'm instantiated");
+
+		if (photonView.isMine)
+		{
+
+		}
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		
+	}
+}
